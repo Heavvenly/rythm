676f529c1c4e3ff99ed673209f88431e699c19b6
Heavvenly <123119089+Heavvenly@users.noreply.github.com>
2025-06-26 15:42:31 -0600

    Update song1.js

--- songs/song1.js
+++ songs/song1.js
@@ -1,6 +1,6 @@
 window.song1 = {
   startDelay: 0, // delay in ms (2 seconds before music starts)
-  audioOffset: 0,
+  audioOffset: 2200,
   songName: "Espresso: Sabrina Carpenter",
   notes: [
   {
